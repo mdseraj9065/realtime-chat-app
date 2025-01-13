c2e7c028fd18c75394ef47b48e0c2578ad419c79
mdseraj9065 <mdseraja341@gmail.com>
2025-01-13 14:52:49 +0530

    Chat App Completed

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const messageSchema = mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+
+    recieverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    text: {
+      type: String,
+    },
+
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+
+const userSchema = mongoose.Schema(
+    {
+        email : {
+            type : String,
+            required : true,
+            unique : true,
+        },
+
+        fullName : {
+            type : String,
+            required : true,
+        },
+
+        password : {
+            type : String,
+            required : true,
+            minlengh : 6,
+        },
+
+        profilePic : {
+            type : String,
+            default : "",
+        },
+    }, 
+    {timestamps : true} 
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
